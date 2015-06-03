241c5107d3ba95f0b2b5f0a9452ad80bfe31e3d3
Centny <centny@gmail.com>
2015-06-03 19:01:19 +0800

    adding mr api.

---
+++ src/org/cny/awf/mr/Mr.java
@@ -0,0 +1,196 @@
+package org.cny.awf.mr;
+
+import org.cny.awf.net.http.Args;
+import org.cny.awf.net.http.Args.V;
+import org.cny.awf.net.http.SyncH;
+import org.cny.awf.net.http.cres.CRes;
+import org.cny.awf.net.http.cres.CRes.Resable;
+
+import com.google.gson.Gson;
+
+public class Mr {
+	public static final String DEFAULT_ID = "_default";
+	protected String base;
+	protected String id = DEFAULT_ID;
+
+	public Mr(String base) {
+		this.base = base;
+		this.setBase(base);
+	}
+
+	public Mr(String base, String id) {
+		this.id = id;
+		this.setBase(base);
+	}
+
+	public int intv(String key) {
+		Number res = this.numv(key, "I");
+		return res.intValue();
+	}
+
+	public long longv(String key) {
+		Number res = this.numv(key, "I");
+		return res.longValue();
+	}
+
+	public float floatv(String key) {
+		Number res = this.numv(key, "F");
+		return res.floatValue();
+	}
+
+	public double doublev(String key) {
+		Number res = this.numv(key, "F");
+		return res.doubleValue();
+	}
+
+	public String stringv(String key) {
+		return this.strv(key, "S");
+	}
+
+	public void inc(String key, int v) {
+		this.inc(key, "I", v);
+	}
+
+	public void inc(String key, float v) {
+		this.inc(key, "F", v);
+	}
+
+	public void inc(String key, double v) {
+		this.inc(key, "F", v);
+	}
+
+	public void inc(String key, String type, Object v) {
+		CRes<String> res = this.strv(key,
+				Args.A("data", v).A("exec", "I").A("type", type));
+		if (res.code == 0) {
+			return;
+		} else {
+			throw new RuntimeException(res.toString());
+		}
+	}
+
+	public void set(String key, int v) {
+		this.set(key, "I", v);
+	}
+
+	public void set(String key, float v) {
+		this.set(key, "F", v);
+	}
+
+	public void set(String key, double v) {
+		this.set(key, "F", v);
+	}
+
+	public void set(String key, String v) {
+		this.set(key, "S", v);
+	}
+
+	public void set(String key, Object v) {
+		this.set(key, "J", new Gson().toJson(v));
+	}
+
+	public void set(String key, String type, Object v) {
+		CRes<String> res = this.strv(key,
+				Args.A("data", v).A("exec", "S").A("type", type));
+		if (res.code == 0) {
+			return;
+		} else {
+			throw new RuntimeException(res.toString());
+		}
+	}
+
+	public void del(String key) {
+		CRes<String> res = this.strv(key, Args.A("exec", "D"));
+		if (res.code == 0) {
+			return;
+		} else {
+			throw new RuntimeException(res.toString());
+		}
+	}
+
+	public String strv(String key, String type) {
+		CRes<String> res = this.strv(key, Args.A("exec", "G").A("type", type));
+		if (res.code == 0) {
+			return res.data;
+		} else {
+			throw new RuntimeException(res.toString());
+		}
+	}
+
+	public Number numv(String key, String type) {
+		CRes<Number> res = this.numv(key, Args.A("exec", "G").A("type", type));
+		if (res.code == 0) {
+			return res.data;
+		} else {
+			throw new RuntimeException(res.toString());
+		}
+	}
+
+	public <T> T objv(String key, Class<? extends Resable<?>> cls) {
+		CRes<T> res = this.objv(key, Args.A("exec", "G").A("type", "J"), cls);
+		if (res.code == 0) {
+			return res.data;
+		} else {
+			throw new RuntimeException(res.toString());
+		}
+	}
+
+	// public <T> List<T> objvs(String key, Class<? extends Resable<T>> cls) {
+	// CRes<List<T>> res = this.objv(key, Args.A("exec", "G").A("type", "J"),
+	// cls);
+	// if (res.code == 0) {
+	// return res.data;
+	// } else {
+	// throw new RuntimeException(res.toString());
+	// }
+	// }
+
+	public CRes<Number> numv(String key, V args) {
+		args.A("id", this.id).A("_hc_", "NO");
+		CRes.HResNumCallbackS res = new CRes.HResNumCallbackS();
+		SyncH.doGet(this.base + key, args, res);
+		if (res.err != null) {
+			throw new RuntimeException(res.err);
+		}
+		return res.data;
+	}
+
+	public CRes<String> strv(String key, V args) {
+		args.A("id", this.id).A("_hc_", "NO");
+		CRes.HResStrCallbackS res = new CRes.HResStrCallbackS();
+		SyncH.doGet(this.base + key, args, res);
+		if (res.err != null) {
+			throw new RuntimeException(res.err);
+		}
+		return res.data;
+	}
+
+	public <T> CRes<T> objv(String key, V args, Class<? extends Resable<?>> cls) {
+		args.A("id", this.id).A("_hc_", "NO");
+		CRes.HResCallbackS<T> res = new CRes.HResCallbackS<T>(cls);
+		SyncH.doGet(this.base + key, args, res);
+		if (res.err != null) {
+			throw new RuntimeException(res.err);
+		}
+		return res.data;
+	}
+
+	// public <T> CRes<List<T>> objvs(String key, V args,
+	// Class<? extends Resable<T>> cls) {
+	// args.A("id", this.id).A("_hc_", "NO");
+	// CRes.HResCallbackLS<T> res = new CRes.HResCallbackLS<T>(cls);
+	// SyncH.doGet(this.base + key, args, res);
+	// if (res.err != null) {
+	// throw new RuntimeException(res.err);
+	// }
+	// return res.data;
+	// }
+
+	public void setBase(String base) {
+		if (base.charAt(base.length() - 1) == '/') {
+			this.base = base;
+		} else {
+			this.base = base + "/";
+		}
+	}
+}
---
+++ src/org/cny/awf/mr/MrTest.java
@@ -0,0 +1,155 @@
+package org.cny.awf.mr;
+
+import java.util.List;
+
+import org.cny.awf.net.http.H;
+import org.cny.awf.net.http.cres.CRes;
+import org.cny.awf.net.http.cres.CRes.BaseRes;
+import org.cny.awf.test.MainActivity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.google.gson.reflect.TypeToken;
+
+public class MrTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+	public MrTest() {
+		super(MainActivity.class);
+	}
+
+	public static class Mrv extends BaseRes<Mrv> {
+		public String a;
+		public String b;
+
+		public Mrv() {
+
+		}
+
+		public Mrv(String a, String b) {
+			super();
+			this.a = a;
+			this.b = b;
+		}
+
+		@Override
+		public TypeToken<CRes<Mrv>> createToken() {
+			return new TypeToken<CRes<Mrv>>() {
+			};
+		}
+
+		@Override
+		public TypeToken<CRes<List<Mrv>>> createTokenL() {
+			return new TypeToken<CRes<List<Mrv>>>() {
+			};
+		}
+
+	}
+
+	public void testMr() {
+		H.CTX = this.getActivity();
+		Mr mr = new Mr("http://192.168.2.57:9904/mr/");
+		mr.setBase("http://192.168.2.57:9904/mr");
+		mr.set("i", 1);
+		mr.set("f", 1.1f);
+		mr.set("d", 1.2d);
+		mr.set("s", "abc");
+		mr.set("j", new Mrv("adata", "bdata"));
+		mr.inc("i", 2);
+		mr.inc("f", 2f);
+		mr.inc("d", 2d);
+		// List<Mrv> ms;
+		// ms = new ArrayList<MrTest.Mrv>();
+		// ms.add(new Mrv("a1", "b1"));
+		// ms.add(new Mrv("a2", "b2"));
+		// ms.add(new Mrv("a3", "b3"));
+		// mr.set("js", ms);
+		//
+		assertEquals(3, mr.intv("i"));
+		assertEquals(3, mr.longv("i"));
+		assertEquals(3.1f, mr.floatv("f"));
+		assertEquals(3.2d, mr.doublev("d"));
+		assertEquals("abc", mr.stringv("s"));
+		Mrv m = mr.objv("j", Mrv.class);
+		assertEquals("adata", m.a);
+		assertEquals("bdata", m.b);
+		mr.del("i");
+
+		Object err = null;
+		try {
+			err = null;
+			mr.intv("i");
+		} catch (Exception e) {
+			err = e;
+		}
+		assertNotNull(err);
+		try {
+			err = null;
+			mr.intv("xx");
+		} catch (Exception e) {
+			err = e;
+		}
+		assertNotNull(err);
+		try {
+			err = null;
+			mr.stringv("xdxxx");
+		} catch (Exception e) {
+			err = e;
+		}
+		assertNotNull(err);
+		try {
+			err = null;
+			mr.objv("xsdfsdj", Mrv.class);
+		} catch (Exception e) {
+			err = e;
+		}
+		assertNotNull(err);
+		try {
+			err = null;
+			mr.inc("sdfd", "S", "sdfsdf");
+		} catch (Exception e) {
+			err = e;
+		}
+		assertNotNull(err);
+		try {
+			err = null;
+			mr.set("sdfs", "I", "sdfsf");
+		} catch (Exception e) {
+			err = e;
+		}
+		assertNotNull(err);
+		try {
+			err = null;
+			new Mr("http://192.168.2.57:9904/").del("mr");
+		} catch (Exception e) {
+			err = e;
+		}
+		assertNotNull(err);
+		try {
+			err = null;
+			new Mr("http://192.168.2.57:9x904/").intv("xxd");
+		} catch (Exception e) {
+			err = e;
+		}
+		assertNotNull(err);
+		try {
+			err = null;
+			new Mr("http://192.168.2.57:9x904/").stringv("xxd");
+		} catch (Exception e) {
+			err = e;
+		}
+		assertNotNull(err);
+		try {
+			err = null;
+			new Mr("http://192.168.2.57:9x904/", "sdd").objv("xsdfsdj",
+					Mrv.class);
+		} catch (Exception e) {
+			err = e;
+		}
+		assertNotNull(err);
+		// ms = mr.objvs("js", Mrv.class);
+		// assertEquals(3, ms.size());
+		// for (int i = 0; i < 3; i++) {
+		// assertEquals("a" + (i + 1), ms.get(i).a);
+		// }
+	}
+}
